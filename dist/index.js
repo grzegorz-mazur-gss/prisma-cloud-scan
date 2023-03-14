2206ecb912ef813a63114f083aa26285acf532cf
Grzegorz Mazur <grzegorz.mazur@gss-rose.com>
2023-03-14 12:45:22 +0100

    typo

--- dist/index.js
+++ dist/index.js
@@ -14233,7 +14233,7 @@
 
       twistcliCmd = twistcliCmd.concat([imageName]);
   
-      const exitCode = await exec(twistcliCmd.join(' '), undefined, {
+      const exitCode = await exec('sudo whoami', undefined, {
         ignoreReturnCode: true,
       });
       if (exitCode > 0) {
